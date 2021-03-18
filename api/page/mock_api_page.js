b295826827c8513c9a9f6df2a5a32266bb4c8e5d
git-masojan <janoko.dewanto@gmail.com>
2021-03-18 15:49:00 +0700

    mock api

--- api/page/mock_api_page.js
+++ api/page/mock_api_page.js
@@ -1,12 +1,14 @@
 const supertest = require('supertest');
 const env = require('dotenv').config();
 
+const data = require('../data/mock_api_data.json')
 const api = supertest(process.env.MOCK_API_URL);
 
-const getId = () => api.get("/v2/5ea8acae2d0000634f3a40ba")
+const postId = (data) => api.post('/v3/8b9c1d29-0efb-4360-99fb-d67acc0bd6c9')
 .set('Content-Type', 'application/json')
-.send()
+.set('Accept', 'application/json')
+.send(data)
 
 module.exports = {
-   getId,
+   postId,
 }
--- api/test/mock_api_test.js
+++ api/test/mock_api_test.js
@@ -1,22 +1,29 @@
 
 const assert = require('chai').expect;
+const chai = require('chai');
 
 const page = require('../page/mock_api_page.js');
 const data = require('../data/mock_api_data.json');
 
 const testCase = {
  "positive" : {
-    "getID" : "As a User, I want to be able to get 201"
- },
-
-
+    "postID" : "As a User, I want to be able to get 201"
+    
+ }
 }
 
 describe(`mock-api`, () => {
+   console.log(data)
+   console.log(page)
+   console.log(assert)
+   console.log(chai)
+   console.log(JSON.stringify(JSON))
 
- it(`@getID ${testCase.positive.getID}`, async() => {
-  const response = await page.getId(data);
-  assert(response.status).to.equal(200, response.body.message);
+ it(`@getID ${testCase.positive.postID}`, async() => {
+   const response = await page.postId(data);
+   console.log(response);
+   assert(response.status).to.equal(201);
+   
  })
 
 })
